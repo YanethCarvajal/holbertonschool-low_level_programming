43de6951c2040380c4a029adc802937f8cc37f5f
YanethCarvajal <yanethcarvajalm@gmail.com>
2022-10-15 09:28:26 -0700

    h4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - print a message using puts
- * return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzze");
+	puts("\"Programming is like building a multilingual puzze"");
 	return (0);
 }
