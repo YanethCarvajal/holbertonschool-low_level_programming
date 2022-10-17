a18f7a3fb5c23866edaf6421718d37ba28ae401b
YanethCarvajal <yanethcarvajalm@gmail.com>
2022-10-17 14:13:58 -0700

    v6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print all single-digit numberz in base 10 starting with 0
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+n = 48;
+while (n <= 57)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
+
+
