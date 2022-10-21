be7427afbe6717be82e6cec7cfabd29d1d17cf03
YanethCarvajal <yanethcarvajalm@gmail.com>
2022-10-20 20:57:28 -0700

    p4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *function that prints a string, in reverse, followed by a new line.
+ *Return: 0
+ *@s: Pointer
+ */
+void print_rev(char *s);
+{
+	int i
+	for(i = '\0'; str[i] != '\0'; i--)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
