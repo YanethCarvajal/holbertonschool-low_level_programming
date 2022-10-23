b37e1dc3a1a2caf7d7045516fc51c3d6ad938075
YanethCarvajal <yanethcarvajalm@gmail.com>
2022-10-22 22:56:40 -0700

    p9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * strcpy - It copies the content into a pointer
+ * _strcpy - It copies the content into a pointer
  * Return: 0
  * @dest: pointer
  * @src: pointer
  */
 char *_strcpy(char *dest, char *src)
 {
-	int y = 0;
+	int x = 0;
 
-	while (src[y])
+	while (src[x])
 	{
-		dest[y] = src[y];
-		y++;
+		dest[x] = src[x];
+		x++;
 	}
-	dest[y] = '\0';
+	dest[x] = '\0';
 
 	return (dest);
 
