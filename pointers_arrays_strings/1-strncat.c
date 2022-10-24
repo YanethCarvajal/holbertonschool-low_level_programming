ec31253a666f8693522c20a369e766f2b8590fb4
YanethCarvajal <yanethcarvajalm@gmail.com>
2022-10-23 19:57:47 -0700

    p1-1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: destination chain
+ * @src: source string
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j <= n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
